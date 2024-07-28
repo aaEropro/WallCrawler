9713b46cad4ef98ab3dece280af633895bec0d8e
aaEropro <143204607+aaEropro@users.noreply.github.com>
2024-07-28 15:02:07 +0300

    stylesheet name or class hot fix

--- src/processor/epub.py
+++ src/processor/epub.py
@@ -4,18 +4,19 @@ from src.mailman.settings import Settings
 
 
 def _replace_tags(tag, italic_classes: list, bold_classes: list, bolditalic_classes: list) -> bool:
+    print(tag.name)
     for item in italic_classes:
-        if item in tag["class"]:
+        if item == tag.name or ("class" in tag.attrs and item == tag["class"]):
             tag.replace_with(f"*{tag.get_text()}*")
             return True
 
     for item in bold_classes:
-        if item in tag["class"]:
+        if item == tag.name or ("class" in tag.attrs and item == tag["class"]):
             tag.replace_with(f"**{tag.get_text()}**")
             return True
 
     for item in bolditalic_classes:
-        if item in tag["class"]:
+        if item == tag.name or ("class" in tag.attrs and item == tag["class"]):
             tag.replace_with(f"***{tag.get_text()}***")
             return True
 
@@ -28,9 +29,9 @@ def process_html(contents: str) -> str:
     soup = BeautifulSoup(contents, 'xml')
 
     for tag in soup.find_all(True):
-        if "class" not in tag.attrs:
-            tag.unwrap()
-            continue
+        # if "class" not in tag.attrs:
+        #     tag.unwrap()
+        #     continue
 
         replaced = _replace_tags(tag, italic_classes, bold_classes, bolditalic_classes)
         if not replaced:
